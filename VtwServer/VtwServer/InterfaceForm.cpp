29dca2a4ebe031afa7a8b1b4eb080a7e46b47de3
PauUPC <recram.viper@gmail.com>
2016-07-27 19:54:18 +0100

    Remake

---
+++ VtwServer/VtwServer/InterfaceForm.cpp
@@ -0,0 +1,15 @@
+#include "InterfaceForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	VtwServer::InterfaceForm form;
+	Application::Run(%form);
+}
---
+++ VtwServer/VtwServer/InterfaceForm.h
@@ -0,0 +1,132 @@
+#pragma once
+
+namespace VtwServer {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de InterfaceForm
+	/// </summary>
+	public ref class InterfaceForm : public System::Windows::Forms::Form
+	{
+	public:
+		InterfaceForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~InterfaceForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::ProgressBar^  progressBar1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: AxWMPLib::AxWindowsMediaPlayer^  axWindowsMediaPlayer1;
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(InterfaceForm::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->progressBar1 = (gcnew System::Windows::Forms::ProgressBar());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->axWindowsMediaPlayer1 = (gcnew AxWMPLib::AxWindowsMediaPlayer());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->axWindowsMediaPlayer1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::SystemColors::ButtonShadow;
+			this->button1->Location = System::Drawing::Point(12, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Start";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &InterfaceForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::SystemColors::ControlDark;
+			this->button2->Location = System::Drawing::Point(12, 41);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Stop";
+			this->button2->UseVisualStyleBackColor = false;
+			// 
+			// progressBar1
+			// 
+			this->progressBar1->Location = System::Drawing::Point(93, 70);
+			this->progressBar1->Name = L"progressBar1";
+			this->progressBar1->Size = System::Drawing::Size(453, 23);
+			this->progressBar1->TabIndex = 2;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(93, 12);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(453, 52);
+			this->textBox1->TabIndex = 3;
+			// 
+			// axWindowsMediaPlayer1
+			// 
+			this->axWindowsMediaPlayer1->Enabled = true;
+			this->axWindowsMediaPlayer1->Location = System::Drawing::Point(12, 99);
+			this->axWindowsMediaPlayer1->Name = L"axWindowsMediaPlayer1";
+			this->axWindowsMediaPlayer1->OcxState = (cli::safe_cast<System::Windows::Forms::AxHost::State^>(resources->GetObject(L"axWindowsMediaPlayer1.OcxState")));
+			this->axWindowsMediaPlayer1->Size = System::Drawing::Size(547, 347);
+			this->axWindowsMediaPlayer1->TabIndex = 4;
+			// 
+			// InterfaceForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(571, 458);
+			this->Controls->Add(this->axWindowsMediaPlayer1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->progressBar1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Name = L"InterfaceForm";
+			this->Text = L"InterfaceForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->axWindowsMediaPlayer1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+};
+}
