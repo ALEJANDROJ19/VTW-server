50a8e32e0f976dc85f542e2d1002bb6bbf0eb60b
ALEJANDROJ19 <alejandroj19@hotmail.es>
2016-07-28 13:06:25 +0200

    new build... again

---
+++ VtwServer/VtwServer/libvpx/third_party/libwebm/mkvparser/mkvreader.cc.d
@@ -0,0 +1,23 @@
+test/../third_party/libwebm/mkvparser/mkvreader.cc.o test/../third_party/libwebm/mkvparser/mkvreader.cc.d: test/../third_party/libwebm/mkvparser/mkvreader.cc \
+ /usr/include/stdc-predef.h \
+ /home/alejandro/Escriptori/gittmp/VTW-server/VtwServer/VtwServer/libvpx/third_party/libwebm/mkvparser/mkvreader.h \
+ /usr/include/c++/4.8/cstdio \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/os_defines.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/cpu_defines.h \
+ /usr/include/stdio.h /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio2.h \
+ /home/alejandro/Escriptori/gittmp/VTW-server/VtwServer/VtwServer/libvpx/third_party/libwebm/mkvparser/mkvparser.h \
+ /usr/include/c++/4.8/cstddef /usr/include/c++/4.8/cassert \
+ /usr/include/assert.h
---
+++ VtwServer/VtwServer/libvpx/vpx_dsp/prob.c.d
@@ -0,0 +1,10 @@
+vpx_dsp/prob.c.o vpx_dsp/prob.c.d: vpx_dsp/prob.c /usr/include/stdc-predef.h vpx_dsp/./prob.h \
+ vpx_config.h vpx_dsp/././vpx_dsp_common.h vpx/vpx_integer.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdint.h /usr/include/stdint.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h /usr/include/inttypes.h \
+ vpx_ports/mem.h vpx_config.h
